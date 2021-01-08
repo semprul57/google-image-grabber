ede11022a09e63402caa71eabe5dc5a5a9cc7dbf
semprul57 <semprul57@gmail.com>
2021-01-08 15:38:59 +0700

    Add keyword suggestion

--- src/GoogleImageGrabber.php
+++ src/GoogleImageGrabber.php
@@ -58,21 +58,18 @@ class GoogleImageGrabber
 		}
 	}
 
-	public static function grab($keyword, $proxy = '', $options = [])
+	public static function suggested_keywords( $keyword )
 	{
-		$url = "https://www.google.com/search?q=" . urlencode($keyword) . "&source=lnms&tbm=isch&tbs=";
+		$url 	=	"https://www.google.com/complete/search?q=". urlencode($keyword) ."&cp=". strlen($keyword) ."&client=img&xssi=t&gs_ri=gws-wiz-img&ds=i&hl=en-US";
 
 		$ua = \Campo\UserAgent::random([
-		    'os_type' => ['Windows', 'OS X'],
+		    'os_type' => ['Windows', 'OS X', 'Linux'],
 		    'device_type' => 'Desktop'
 		]);
-
-        if(!empty($proxy)) $proxy = "tcp://$proxy";
 
 		$options  = [
 			'http' => [
 				'method'     =>"GET",
-				'proxy'           => "$proxy",
 				'user_agent' =>  $ua,
 			],
 			'ssl' => [
@@ -80,6 +77,41 @@ class GoogleImageGrabber
 				"verify_peer_name" => FALSE,
 			],
 		];
+
+		$context  = stream_context_create($options);
+		$response = file_get_contents($url, FALSE, $context);
+
+		$data 	= 	preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+					  return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+					}, $response);
+		$data 	= 	strip_tags( $data );
+
+		$output = 	[];
+		$data 	= 	preg_match_all('/"(.*?)"/', $data, $output);
+
+		return $output;
+	}
+
+	public static function grab($keyword, $options = [])
+	{
+		$url = "https://www.google.com/search?q=" . urlencode($keyword) . "&source=lnms&tbm=isch&tbs=";
+
+		$ua = \Campo\UserAgent::random([
+		    'os_type' => ['Windows', 'OS X', 'Linux'],
+		    'device_type' => 'Desktop'
+		]);
+
+		$options  = [
+			'http' => [
+				'method'     =>"GET",
+				'user_agent' =>  $ua,
+			],
+			'ssl' => [
+				"verify_peer"      => FALSE,
+				"verify_peer_name" => FALSE,
+			],
+		];
+
 		$context  = stream_context_create($options);
 
 		$response = file_get_contents($url, FALSE, $context);
@@ -94,7 +126,6 @@ class GoogleImageGrabber
 
 		$data = json_decode($data, true);
 
-
 		$rawResults = [];
 		$results = [];
 
@@ -107,33 +138,30 @@ class GoogleImageGrabber
 
 			self::filterResult($rawResult, $result);
 			$data = self::getValues($result);
-			
+
+
 			$result = [];
 
 			if(count($data) >= 11){
-
 			    $result['keyword'] = $keyword;
 			    $result['slug'] = __::slug($keyword);
 
-			    $result['title'] = isset($data[13]) ? ucwords(__::slug($data[13], ['delimiter' => ' '])) : 'none';
-			    $result['alt'] = isset($data[19]) ? __::slug($data[19], ['delimiter' => ' ']) : 'none';
+			    $result['title'] = isset($data[13]) ? ucwords(__::slug($data[13], ['delimiter' => ' '])) : null;
+			    $result['alt'] = isset($data[19]) ? __::slug($data[19], ['delimiter' => ' ']) : null;
 			    
-			    $result['url'] = $data[8];
-			    $result['filetype'] = self::getFileType($data[8]);
-			    $result['width'] = $data[6];
-			    $result['height'] = $data[7];
-			    $result['source'] = isset($data[12]) ? $data[12] : 'none';
-			    $result['domain'] = isset($data[20]) ? $data[20] : 'none';
-			    $result['thumbnail'] =  isset($data[26]) ? $data[26] : $data[1];
+			    $result['url'] = isset($data[8]) ? $data[8] : null;
+			    $result['filetype'] = self::getFileType(isset($data[8]) ? $data[8] : '');
+			    $result['width'] = isset($data[6]) ? $data[6] : null;
+			    $result['height'] = isset($data[7]) ? $data[7] : null;
+			    $result['source'] = isset($data[12]) ? $data[12] : null;
+			    $result['domain'] = isset($data[20]) ? $data[20] : null;
 
-			    if(strpos($result['url'], 'http') !== false){
-			    	
-					$results[] = $result;
-			    }
-
+				$results[] = $result;
 			}
 
 		}
+
+		sleep(5);
 
 		return $results;
 	}
@@ -160,7 +188,7 @@ class GoogleImageGrabber
 				break;
 			
 			default:
-				return 'jpg';
+				return 'png';
 				break;
 		}
 	}
